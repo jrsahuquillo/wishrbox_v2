ecbba7d57b3767d21be5517635150ddf276c6305
jrsahuquillo <jrsahuquillo@gmail.com>
2018-06-18 23:16:39 +0200

    Sets up login user and some refactors

---
+++ spec/features/sign_in_users_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature "Sign in users" do
+
+  before do
+    @sahu = User.create!(email: 'sahu@mail.com', password: 'password')
+  end
+
+  scenario "with valid credentials" do
+    visit "/"
+    page.find('a.desktop-login').click
+    fill_in "Email", with: @sahu.email
+    fill_in "Password", with: @sahu.password
+    click_button "Log in"
+
+    expect(page.current_path).to eq("/")
+    expect(page).to have_css("#toast-container")
+    expect(page).not_to have_css('a.desktop-login')
+    expect(page).not_to have_css('a.mobile-login')
+    expect(page).not_to have_css('a.desktop-signup')
+    expect(page).not_to have_css('a.mobile-signup')
+  end
+
+  scenario "with invalid credentials" do
+    visit "/"
+    page.find('a.desktop-login').click
+    fill_in "Email", with: ""
+    fill_in "Password", with: ""
+    click_button "Log in"
+
+    expect(page.current_path).to eq("/users/sign_in")
+    expect(page).to have_css("#toast-container")
+
+  end
+end
