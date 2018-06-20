fc2b5a99fef5ca097e9e488ea89d4aff1fdf10c2
jrsahuquillo <jrsahuquillo@gmail.com>
2018-06-20 22:58:12 +0200

    Tests that a user can not edit a wish created by other user

--- spec/features/edit_wish_spec.rb
+++ spec/features/edit_wish_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.feature "Editing Wishes" do
 
   before do
-    sahu = User.create!(email: 'sahu@mail.com', password: 'password')
-    login_as(sahu)
+    sahu = User.create!(username: 'sahu', email: 'sahu@mail.com', password: 'password')
     @wish = Wish.create(title: "Wish1", description: "Wish1 description", user: sahu)
+    login_as(sahu)
   end
 
   scenario "A user updates a wish" do
@@ -20,6 +20,7 @@ RSpec.feature "Editing Wishes" do
     # expect(page).to have_content("Wish has been updated")
     expect(page).to have_css("#toast-container")
     expect(page.current_path).to eq(wishes_path)
+    expect(page).to have_css(".modal-footer #modal1", text: 'edit')
   end
 
   scenario "A user fails to update a wish" do
@@ -33,6 +34,19 @@ RSpec.feature "Editing Wishes" do
 
     expect(page).to have_content("Wish Title can't be blank")
     expect(page.current_path).to eq(wish_path(@wish))
+  end
+
+  scenario "A user can not update another user wish" do
+    user1 = User.create!(username: 'user1', email: 'user1@mail.com', password: 'password')
+    @wish1 = Wish.create(title: "Wish1", description: "Wish1 description", user: user1)
+    user2 = User.create!(username: 'user2', email: 'user2@mail.com', password: 'password')
+    @wish2 = Wish.create(title: "Wish2", description: "Wish2 description", user: user2)
+    login_as(user1)
+
+    visit "/"
+    click_link @wish2.title
+
+    expect(page).not_to have_css(".modal-footer #modal1", text: 'edit')
   end
 
 end
