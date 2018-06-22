866584afb4cfa528e81d132278a2f3518e4b48b8
jrsahuquillo <jrsahuquillo@gmail.com>
2018-06-22 21:58:31 +0200

    Changes seed generation

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-12.times do |i|
+usernames = ['Sahu', 'Carlos', 'Alvaro', 'Filipa', 'Daichi', 'Simon', 'Miguel Angel', 'Juan Pablo', 'Diana', 'Mariano']
+domain = ['gmail.com', 'mail.com', 'hotmail.com', 'yahoo.es']
+wishes = ['iPhone 7', 'iPad 2 Air', 'MackBook Pro', 'Apple TV', 'MacBook Air', 'Apple Watch', 'Earpods', 'Apple Music', 'iPod touch', 'Mac mini' ]
 
-  Wish.create!({
-		title: "Wish#{i}",
-		description: "The very best wish ever in the world"
+10.times do |i|
+	username = usernames[i]
+	wish = wishes[i]
+
+	user = User.create!({
+		username: username,
+		email: username.gsub(/\s+/, "").downcase+"@"+domain[rand(domain.length)],
+		password: '12345678',
+		password_confirmation:'12345678',
+		})
+
+	user.wishes.create!({
+		title: wish,
+		description: "The very best #{wish} ever in the world",
 		})
 
 end
