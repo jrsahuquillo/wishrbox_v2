583d05d33ccfa0b12f4eb8448d3b3440ee866531
jrsahuquillo <jrsahuquillo@gmail.com>
2018-06-25 23:36:10 +0200

    Corrects show_wish_spec

--- spec/features/show_wish_spec.rb
+++ spec/features/show_wish_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 RSpec.feature "Showing a wish" do
 
   before do
-    sahu = User.create!(email: 'sahu@mail.com', password: 'password')
+    sahu = User.create!(username: 'sahuqui', email: 'sahu@mail.com', password: 'password')
     login_as(sahu)
     @wish = Wish.create(title: "Wish1", description: "Wish1 description", user: sahu)
   end
 
   scenario 'A user shows a wish in a modal' do
     visit '/'
-    page.find('a#modal' + @wish.id.to_s).click
+
+    page.find('#modal' + (@wish.id.to_s)).click
 
     expect(page).to have_content(@wish.title)
     expect(page).to have_content(@wish.description)
