aa67c5fb487bc0f06515a2f595eabfe9c68d0037
jrsahuquillo <jrsahuquillo@gmail.com>
2018-08-17 21:53:29 +0200

    Implements validations to creating new wishlists

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,4 +1,10 @@
 class Wishlist < ApplicationRecord
+  validates :title, presence: true
+  validates_uniqueness_of :title
+  validates_length_of :title, minimum: 3, maximum: 30, allow_blank: false
+
+  default_scope { order(created_at: :desc) }
+
   has_many :user_wishlists
   has_many :users, through: :user_wishlists
 
--- spec/features/create_wishlist_spec.rb
+++ spec/features/create_wishlist_spec.rb
@@ -5,11 +5,13 @@ RSpec.feature "Creating Wishlists" do
   before do
     @sahu = User.create!(email: 'sahu@mail.com', password: 'password')
     login_as(@sahu)
+
+    visit "/"
+    click_link "New Wishlist"
   end
 
   scenario "A user creates a new wishlist" do
-    visit "/"
-    click_link "New Wishlist"
+
     fill_in "Title", with: "Wishlist1"
     fill_in "Description", with: "Lorem ipsum wishlist 1"
     click_button "Create Wishlist"
@@ -19,5 +21,53 @@ RSpec.feature "Creating Wishlists" do
     expect(page.current_path).to eq(wishlists_path)
     expect(page).to have_css("#toast-container")
 
+  end
+
+  scenario "A user fails to create a untitled wishlist" do
+
+    fill_in "Title", with: ""
+    fill_in "Description", with: "Lorem ipsum wishlist 1"
+    click_button "Create Wishlist"
+
+    expect(page).to have_content("Title can't be blank")
+    expect(page.current_path).to eq(wishlists_path)
+    expect(page).to have_css("#toast-container")
+  end
+
+  scenario "A user fails to create a wishlist with repeated title" do
+
+    fill_in "Title", with: "Wishlist 1"
+    fill_in "Description", with: "Lorem ipsum wishlist 1"
+    click_button "Create Wishlist"
+    click_link "New Wishlist"
+    fill_in "Title", with: "Wishlist 1"
+    fill_in "Description", with: "Lorem ipsum wishlist 1"
+    click_button "Create Wishlist"
+
+    expect(page).to have_content("Title has already been taken")
+    expect(page.current_path).to eq(wishlists_path)
+    expect(page).to have_css("#toast-container")
+  end
+
+  scenario "A user fails to create a wishlist with more than 30 characters in title" do
+
+    fill_in "Title", with: "This a wish list with more than 30 characters"
+    fill_in "Description", with: "Lorem ipsum wishlist 1"
+    click_button "Create Wishlist"
+
+    expect(page).to have_content("Title is too long (maximum is 30 characters)")
+    expect(page.current_path).to eq(wishlists_path)
+    expect(page).to have_css("#toast-container")
+  end
+
+  scenario "A user fails to create a wishlist with less than 3 characters in title" do
+
+    fill_in "Title", with: "W"
+    fill_in "Description", with: "Lorem ipsum wishlist 1"
+    click_button "Create Wishlist"
+
+    expect(page).to have_content("Title is too short (minimum is 3 characters)")
+    expect(page.current_path).to eq(wishlists_path)
+    expect(page).to have_css("#toast-container")
   end
 end
